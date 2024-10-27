620ceb2806242c37156978187031fb4c81685078
Jack Dibben <jd892@kent.ac.uk>
2024-10-27 23:40:28 +0000

    part 2

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,15 @@
 ### Part Two -- your code goes here. 
+import random
+
+random_number = random.randint(1, 10)
+
+def number_guesser():
+    guess = int(input("Guess the random number between 1 and 100: "))
+    while guess == random_number:
+        print("Correct!")
+        break
+    else:
+        print("Incorrect. Guess again.")
+        number_guesser()
+
+number_guesser()
